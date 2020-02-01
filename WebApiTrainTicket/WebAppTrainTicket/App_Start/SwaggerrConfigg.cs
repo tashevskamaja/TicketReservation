aa87f53a19b7ee1e5ceb49dd2593f87c59845c02
Maja Tashevska <tashevskamaja@gmail.com>
2020-02-01 11:51:55 +0100

    initial projecT

---
+++ WebApiTrainTicket/WebAppTrainTicket/App_Start/SwaggerrConfigg.cs
@@ -0,0 +1,26 @@
+﻿using System.Web.Http;
+using WebActivatorEx;
+using Swashbuckle.Application;
+using System;
+using OnlineTrainTicketReservation;
+
+
+[assembly: PreApplicationStartMethod(typeof(SwaggerConfig), "Register")]
+
+namespace WebAppTrainTicket.App_Start
+{
+    public class SwaggerrConfigg
+    {
+        public static void Register()
+        {
+            var thisAssembly = typeof(SwaggerrConfigg).Assembly;
+
+            GlobalConfiguration.Configuration
+                .EnableSwagger(c =>
+                {
+                    c.SingleApiVersion("v1", "WebAppTrainTicket");
+                })
+                .EnableSwaggerUi();
+        }
+    }
+}
